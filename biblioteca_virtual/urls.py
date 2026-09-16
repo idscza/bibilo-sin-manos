973bd6a4a2bd00eb2ef28b88dfb551eb3f866a76
Ivan Salazar <id.salazar@uniandes.edu.co>
2026-09-16 16:01:49 -0500

    landing-fixed

--- biblioteca_virtual/urls.py
+++ biblioteca_virtual/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from usuarios import views
+from libros import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
